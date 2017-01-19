c67c4fe82a57606fd87976e5807bf18dfa3bbb94
Katsuyoshi Endou <endkatsu@vesta.ocn.ne.jp>
2017-01-19 21:43:11 +0900

    結果詳細ページ作成、ページの遷移を少し

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -1,45 +1,20 @@
 package controllers;
 
-import java.util.ArrayList;
+import java.net.URLDecoder;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.inject.Inject;
-
 import com.avaje.ebean.Ebean;
-import com.avaje.ebean.Expr;
-import com.avaje.ebean.ExpressionList;
-import com.avaje.ebean.SqlQuery;
 import com.avaje.ebean.SqlRow;
-import com.typesafe.config.ConfigFactory;
-
 import controllers.dbaccess.DataAccess;
-import models.Course;
-import models.Distance;
-import models.Location;
-import models.Race;
-import models.Rank;
-import models.Sample;
-import models.custom.RaceForecastInfo;
-import models.custom.RaceResultInfo;
-import play.Logger;
-import play.api.Play;
-import play.libs.Json;
-import play.libs.mailer.Email;
-import play.libs.mailer.MailerClient;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.forecast;
-import views.html.input;
 import views.html.keibaweb;
-import views.html.list;
-import views.html.result;
 import views.html.calendar;
 import views.html.raceinfo;
 import views.html.raceresult;
+import views.html.horseinfo;
 
 public class ApplicationController extends Controller {
     public Result showKeibaweb() {
@@ -59,96 +34,12 @@ public class ApplicationController extends Controller {
         return ok(keibaweb.render(information));
     }
 
-    public Result showInputPage() {
-        List<Location> location = Location.find.findList();
-        List<Rank> rank = Rank.find.findList();
-        List<Course> course = Course.find.findList();
-        List<Distance> distance = Distance.find.findList();
+	public Result showRaceCalendar(String year) {
+		if(year == null || "".equals(year)) {
+			Calendar cal = Calendar.getInstance();
 
-        return ok(input.render(location, rank, course, distance));
-	}
-
-    public Result getRaceCompIndex() {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("select b.race_id,a.horse_number_1, a.comp_index_1,a.horse_number_2, a.comp_index_2,a.horse_number_3, a.comp_index_3,a.horse_number_4, a.comp_index_4,a.horse_number_5, a.comp_index_5, ");
-        sb.append("a.horse_number_6, a.comp_index_6,a.horse_number_7, a.comp_index_7,a.horse_number_8, a.comp_index_8,a.horse_number_9, a.comp_index_9,a.horse_number_10, a.comp_index_10, ");
-        sb.append("a.horse_number_11, a.comp_index_11,a.horse_number_12, a.comp_index_12,a.horse_number_13, a.comp_index_13,a.horse_number_14, a.comp_index_14,a.horse_number_15, a.comp_index_15, ");
-        sb.append("a.horse_number_16, a.comp_index_16,a.horse_number_17, a.comp_index_17,a.horse_number_18, a.comp_index_18 ");
-        sb.append("from tr_entry_horse_info as a, tr_race as b ");
-        sb.append("where b.open_date = to_date(:openDate,'YYYY-MM-DD') and b.location_kbn = :locationKbn and a.race_id = b.race_id order by b.race_number");
-
-        String sql = sb.toString();
-
-        SqlQuery query = Ebean.createSqlQuery(sql);
-        query.setParameter("openDate", "2015-12-20");
-        query.setParameter("locationKbn", "5");
-        List<SqlRow> result = query.findList();
-
-        List<RaceForecastInfo> items = new ArrayList<RaceForecastInfo>();
-        for(int i = 0; i < result.size(); i++) {
-            RaceForecastInfo rfi = new RaceForecastInfo();
-            rfi.setRaceForecastInfo(result.get(i));
-
-            items.add(rfi);
-        }
-        return ok(Json.toJson(items));
-    }
-
-    public Result showForecastPage() {
-        return ok(forecast.render(null));
-    }
-
-    public Result getRaceResult() {
-    	StringBuilder sb = new StringBuilder();
-
-    	sb.append("select b.open_date, c.location_name, b.race_number, d.rank_name, e.course_name, f.distance_name, b.race_population, ");
-    	sb.append("a.* ");
-    	sb.append("from tr_race_result as a, tr_race as b, ms_location as c, ms_rank as d, ms_course as e, ms_distance as f ");
-    	sb.append("where a.race_id = b.race_id and b.location_kbn = c.location_kbn and b.rank_kbn = d.rank_kbn and b.course_kbn = e.course_kbn and b.distance_kbn = f.distance_kbn ");
-    	sb.append("and open_date > '2015/8/29' ");
-    	sb.append("order by b.open_date, b.location_kbn, b.race_number");
-
-    	String sql = sb.toString();
-
-    	List<SqlRow> results = Ebean.createSqlQuery(sql).findList();
-
-    	List<RaceResultInfo> rri = new ArrayList<RaceResultInfo>();
-    	for(SqlRow result : results) {
-    		RaceResultInfo info = new RaceResultInfo();
-
-    		info.setRaceRsultInfo(result);
-
-    		rri.add(info);
-    	}
-    	return ok(Json.toJson(rri));
-    }
-
-    public Result showResultPage() {
-    	return ok(result.render(null));
-	}
-
-    public Result showListPage() {
-    	StringBuilder sb = new StringBuilder();
-
-    	sb.append("select b.open_date, c.location_name, b.race_number, d.rank_name, e.course_name, f.distance_name, b.race_population, ");
-    	sb.append("a.* ");
-    	sb.append("from tr_race_result as a, tr_race as b, ms_location as c, ms_rank as d, ms_course as e, ms_distance as f ");
-    	sb.append("where a.race_id = b.race_id and b.location_kbn = c.location_kbn and b.rank_kbn = d.rank_kbn and b.course_kbn = e.course_kbn and b.distance_kbn = f.distance_kbn ");
-    	sb.append("and open_date >= '2015/8/29' ");
-    	sb.append("order by b.open_date, b.location_kbn, b.race_number");
-
-    	String sql = sb.toString();
-
-    	List<SqlRow> results = Ebean.createSqlQuery(sql).findList();
-
-    	return ok(list.render(results));
-    }
-
-	public Result showRaceCalendar() {
-		Calendar cal = Calendar.getInstance();
-
-		String year = String.valueOf(cal.get(Calendar.YEAR) - 1);
+			year = String.valueOf(cal.get(Calendar.YEAR) - 1);
+		}
 
 		List<SqlRow> result = DataAccess.getOpenYear();
 		List<SqlRow> opendates = DataAccess.getOpenDateByYear(year);
@@ -182,8 +73,25 @@ public class ApplicationController extends Controller {
 	}
 
 	public Result showRaceResult(String id) {
-		List<SqlRow>result = DataAccess.getRaceResultDetails(Integer.valueOf(id));
+		List<SqlRow> result = DataAccess.getRaceResultDetails(Integer.valueOf(id));
+		List<SqlRow> payout = DataAccess.getRaceDivident(Integer.valueOf(id));
+		List<SqlRow> info = DataAccess.getRaceInfomationById(Integer.valueOf(id));
 
-		return ok(raceresult.render(result));
+		return ok(raceresult.render(info, result, payout));
+	}
+
+	public Result showHorseInfo(String name) {
+		String method = request().method();
+
+		List<SqlRow> result = null;
+		if("GET".equals(method)) {
+			Map<String, String[]> req = request().queryString();
+
+			String value = req.get("name")[0];
+			String param = URLDecoder.decode(value);
+
+			result = DataAccess.getHorseInfo(param);
+		}
+		return ok(horseinfo.render(result));
 	}
 }
--- app/controllers/dbaccess/DataAccess.java
+++ app/controllers/dbaccess/DataAccess.java
@@ -58,7 +58,15 @@ public class DataAccess {
 
 		sb.append("select ");
 		sb.append("  a.id, ");
+		sb.append("  to_char(a.open_date, 'yyyy年mm月dd日') as open_date, ");
+		sb.append("  i.location_name, ");
 		sb.append("  a.race_no, ");
+		sb.append("  case ");
+		sb.append("    when a.race_grade = '1' then 'G1' ");
+		sb.append("    when a.race_grade = '2' then 'G2' ");
+		sb.append("    when a.race_grade = '3' then 'G3' ");
+		sb.append("    else '' ");
+		sb.append("  end as race_grade, ");
 		sb.append("  b.age_name, ");
 		sb.append("  d.rank_name, ");
 		sb.append("  a.race_name, ");
@@ -88,7 +96,7 @@ public class DataAccess {
 		sb.append("    else '' ");
 		sb.append("  end as type_6, ");
 		sb.append("  a.population, ");
-		sb.append("  f.distance_from, ");
+		sb.append("  a.course_distance, ");
 		sb.append("  substr(h.course_name, 1, 1) as course_name ");
 		sb.append("from tr_race as a ");
 		sb.append("  left join ms_age as b on a.age_kbn = b.age_kbn ");
@@ -96,6 +104,7 @@ public class DataAccess {
 		sb.append("  left join ms_handicap as e on a.handi_kbn = e.handi_kbn ");
 		sb.append("  left join ms_distance as f on a.distance_kbn = f.distance_kbn ");
 		sb.append("  left join ms_course as h on a.course_kbn = h.course_kbn ");
+		sb.append("  left join ms_location as i on a.location_kbn = i.location_kbn ");
 		sb.append("where a.open_date = to_date(:openDate, 'yyyy-mm-dd') ");
 		sb.append("and a.location_kbn = :locationKbn ");
 		sb.append("order by length(a.race_no), a.race_no ");
@@ -146,5 +155,93 @@ public class DataAccess {
 		String sql = sb.toString();
 
 		return Ebean.createSqlQuery(sql).setParameter("raceId", raceId).findList();
+	}
+
+	public static List<SqlRow> getRaceDivident(int raceId) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("select ");
+		sb.append("  b.divident_name, ");
+		sb.append("  a.win_number, ");
+		sb.append("  to_char(a.payout, 'FM999,999,999') as payout, ");
+		sb.append("  a.popularity ");
+		sb.append("from tr_race_divident as a ");
+		sb.append("  left join ms_divident as b on a.divident_kind = b.divident_kind ");
+		sb.append("where tr_race_id = :raceId ");
+		sb.append("order by a.divident_kind ");
+
+		String sql = sb.toString();
+
+		return Ebean.createSqlQuery(sql).setParameter("raceId", raceId).findList();
+	}
+
+	public static List<SqlRow> getRaceInfomationById(int id) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("select ");
+		sb.append("  to_char(a.open_date, 'yyyy年mm月dd日') as open_date, ");
+		sb.append("  a.race_no, ");
+		sb.append("  j.location_name, ");
+		sb.append("  b.age_name, ");
+		sb.append("  d.rank_name, ");
+		sb.append("  a.race_name, ");
+		sb.append("  e.handi_name, ");
+		sb.append("  a.population, ");
+		sb.append("  substr(f.course_name,1,1) as course_name, ");
+		sb.append("  a.course_distance, ");
+		sb.append("  h.weather_name, ");
+		sb.append("  i.condition_name ");
+		sb.append("from tr_race as a ");
+		sb.append("  left join ms_age as b on a.age_kbn = b.age_kbn ");
+		sb.append("  left join ms_rank as d on a.rank_kbn = d.rank_kbn ");
+		sb.append("  left join ms_handicap as e on a.handi_kbn = e.handi_kbn ");
+		sb.append("  left join ms_course as f on a.course_kbn = f.course_kbn ");
+		sb.append("  left join ms_weather as h on a.weather_kbn = h.weather_kbn ");
+		sb.append("  left join ms_condition as i on a.condition_kbn = i.condition_kbn ");
+		sb.append("  left join ms_location as j on a.location_kbn = j.location_kbn ");
+		sb.append("where a.id = :id");
+
+		String sql = sb.toString();
+
+		return Ebean.createSqlQuery(sql).setParameter("id", id).findList();
+	}
+
+	public static List<SqlRow> getHorseInfo(String name) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("select ");
+		sb.append("  a.tr_race_id, ");
+		sb.append("  b.open_date, ");
+		sb.append("  d.location_name, ");
+		sb.append("  b.race_no, ");
+		sb.append("  a.horse_name, ");
+		sb.append("  (o.sex_name || a.age) as horse_sex_age, ");
+		sb.append("  a.race_order, ");
+		sb.append("  e.age_name, ");
+		sb.append("  f.rank_name, ");
+		sb.append("  b.race_name, ");
+		sb.append("  h.handi_name, ");
+		sb.append("  a.basis_weight, ");
+		sb.append("  substr(i.course_name, 1, 1) as course_name, ");
+		sb.append("  b.course_distance, ");
+		sb.append("  j.weather_name, ");
+		sb.append("  n.condition_name, ");
+		sb.append("  a.stable ");
+		sb.append("from tr_race_result as a, tr_race as b, ms_location as d, ms_age as e, ms_rank as f, ms_handicap as h, ms_course as i, ms_weather as j, ms_condition as n, ms_horse_sex as o ");
+		sb.append("where a.tr_race_id = b.id ");
+		sb.append("and a.horse_name = :name ");
+		sb.append("and a.location_kbn = d.location_kbn ");
+		sb.append("and b.age_kbn = e.age_kbn ");
+		sb.append("and b.rank_kbn = f.rank_kbn ");
+		sb.append("and b.handi_kbn = h.handi_kbn ");
+		sb.append("and b.course_kbn = i.course_kbn ");
+		sb.append("and b.weather_kbn = j.weather_kbn ");
+		sb.append("and b.condition_kbn = n.condition_kbn ");
+		sb.append("and a.sex = o.horse_sex_kbn ");
+		sb.append("order by b.open_date desc");
+
+		String sql = sb.toString();
+
+		return Ebean.createSqlQuery(sql).setParameter("name",name).findList();
 	}
 }
